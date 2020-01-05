c107f21efc1cbec6bb6f498bb579a5e6a2fa618f
ariflateefshah <arif@ideas2it.com>
2020-01-05 22:56:16 +0530

    Feature: Class Binding in Angular8

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -8,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
     div{
         color:red
     }
+    .text-success{
+      color:green
+    }
+    .text-warning{
+      color:red
+    }
+    .text-info{
+      color:gold
+    }
+    .text-special{
+      font-style:italic
+    }
   `]
 })
 export class TestComponent implements OnInit {
@@ -15,6 +27,14 @@ export class TestComponent implements OnInit {
   public myId ="myTestId";
   public isDisabled ="true"
   public siteUrl =window.location.href;
+  public successClass= "text-success";
+  public hasError =true;
+  public isSpecial = true;
+  public messageClasses = {
+      "text-success": !this.hasError,
+      "text-warning": this.hasError,
+      "text-special": this.isSpecial
+  }
   constructor() { }
 
   ngOnInit() {
