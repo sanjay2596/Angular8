d2bc4555a105037d43c6442169861d5238a139c8
sanjay2596 <sanjay@ideas2it.com>
2020-06-30 23:17:31 +0530

    crud for blog

--- hello-world/src/app/app-routing.module.ts
+++ hello-world/src/app/app-routing.module.ts
@@ -5,12 +5,17 @@ import { LoginComponent } from './login/login.component';
 import { CommentsComponent } from './components/comments/comments.component';
 import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
 import { BlogInnerComponent } from './components/blog-inner/blog-inner.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-{path: '', redirectTo:'/home', pathMatch: 'full'},
 {path:'home', component:AppLayoutComponent},
-{path:'blog/:id',component:BlogInnerComponent}
+{path:'blog/:id',component:BlogInnerComponent},
+{ path:"",component:HomeComponent},
+{ path:"add-blog",component:AddBlogComponent},
+{ path:":id", component:BlogViewComponent }
 ];
 
 @NgModule({
